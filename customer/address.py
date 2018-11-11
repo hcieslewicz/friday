0ce15397e7b1c8c1c2b298bce5ee8126b3ca083f
hubi <hubcio44@wp.pl>
2018-11-11 14:09:26 +0100

    add regexes to find street name and number. This state has 8 from 9 tests passed.

--- customer/address.py
+++ customer/address.py
@@ -1,6 +1,39 @@
 import json
+import re
 
 class Address():
     def split_street_name_and_number(self, combined_street):
-        return json.dumps({"street": "Winterallee", "housenumber": "3"}, ensure_ascii=False)
-        #pass
+        print "string to work with: %s" % combined_street
+        combined_street = combined_street.strip()
+
+        regex_list = [r'^(?P<street>.*?\D),? (?P<house>\d+\s?\w?)$',
+                      r'^(?P<street>.*?\D[\s?\D?]+) (?P<house>\d+\s?\w?)$',
+                      r'^(?P<house>\d+)[\s|,]\s?(?P<street>[\s*\w*]+)$']
+
+        for regex in regex_list:
+            print "regex: %s" % regex
+            m = re.search(regex, combined_street)
+            if m:
+                print "m.group(0): %s" % m.group(0)
+                #print "m.group(1): %s" % m.group(1)
+                #print "m.group(2): %s" % m.group(2)
+                #street_name = m.group(1)
+                #house_number = m.group(2)
+                street_name = m.group('street')
+                house_number = m.group('house')
+                print "street_name: %s" % street_name
+                print "house_number: %s" % house_number
+                break
+
+
+
+
+        a = {}
+        a["street"] = street_name
+        a["housenumber"] = house_number
+        return json.dumps(a, ensure_ascii=False)
+
+
+
+
+
--- tests/test_address.py
+++ tests/test_address.py
@@ -1,8 +1,22 @@
-import json
+# coding=utf-8
+import sys
+from os import path
+#sys.path.append( path.dirname( path.dirname( path.abspath(__file__) ) ) )
+import unittest
 from context import customer
+import json
+
 
 
 def test_address_line_splitting():
     addr = customer.address.Address()
-    print "text to split: %s" % addr.split_street_name_and_number("Winterallee 3")
+    print "aaa: %s" % addr.split_street_name_and_number("Winterallee 3")
     assert addr.split_street_name_and_number("Winterallee 3") == json.dumps({"street": "Winterallee", "housenumber": "3"}, ensure_ascii=False)
+    assert addr.split_street_name_and_number("Musterstrasse 45") == json.dumps({"street": "Musterstrasse", "housenumber": "45"}, ensure_ascii=False)
+    assert addr.split_street_name_and_number("Blaufeldweg 123B") == json.dumps({"street": "Blaufeldweg", "housenumber": "123B"}, ensure_ascii=False)
+    assert addr.split_street_name_and_number("Am Bächle 23") == json.dumps({"street": "Am Bächle", "housenumber": "23"}, ensure_ascii=False)
+    assert addr.split_street_name_and_number("Auf der Vogelwiese 23 b") == json.dumps({"street": "Auf der Vogelwiese", "housenumber": "23 b"}, ensure_ascii=False)
+    assert addr.split_street_name_and_number("4, rue de la revolution") == json.dumps({"street": "rue de la revolution", "housenumber": "4"}, ensure_ascii=False)
+    assert addr.split_street_name_and_number("200 Broadway Av") == json.dumps({"street": "Broadway Av", "housenumber": "200"}, ensure_ascii=False)
+    assert addr.split_street_name_and_number("Calle Aduana, 29") == json.dumps({"street": "Calle Aduana", "housenumber": "29"}, ensure_ascii=False)
+    assert addr.split_street_name_and_number("Calle 39 No 1540") == json.dumps({"street": "Calle 39", "housenumber": "No 1540"}, ensure_ascii=False)
